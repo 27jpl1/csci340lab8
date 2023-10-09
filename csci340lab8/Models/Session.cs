46162a0ab112bb40e643fc6c079f11af409c3653
JP Lyon <japylyon@gmail.com>
2023-10-09 14:46:21 -0500

    Updated Display Names

--- csci340lab8/Models/Session.cs
+++ csci340lab8/Models/Session.cs
@@ -5,12 +5,16 @@ namespace RazorPagesMovie.Models;
 public class Session
 {
     public int Id { get; set; }
+    [Display(Name = "Game Title")]
     public string? GameTitle { get; set; }
     [DataType(DataType.Date)]
     public DateTime Date { get; set; }
+    [Display(Name = "Start Time")]
     public TimeOnly StartTime {get; set;}
+    [Display(Name = "End Time")]
     public TimeOnly EndTime {get; set;}
     public int Wins { get; set; }
     public int Losses{get; set;}
+    [Display(Name = "Rank Change")]
     public string? RankChange { get; set; }
 }
